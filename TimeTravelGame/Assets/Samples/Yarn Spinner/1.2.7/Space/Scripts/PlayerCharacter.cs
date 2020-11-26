f748e019b42a149fbf4ae8bce520ce2567800ba7
Ben Shackman <Bshackman@protonmail.com>
2020-11-26 14:20:20 -0500

    Added basic SaveState system

--- TimeTravelGame/Assets/Samples/Yarn Spinner/1.2.7/Space/Scripts/PlayerCharacter.cs
+++ TimeTravelGame/Assets/Samples/Yarn Spinner/1.2.7/Space/Scripts/PlayerCharacter.cs
@@ -28,8 +28,10 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace Yarn.Unity.Example {
-    public class PlayerCharacter : MonoBehaviour {
+namespace Yarn.Unity.Example
+{
+    public class PlayerCharacter : MonoBehaviour
+    {
 
         public float minPosition = -5.3f;
         public float maxPosition = 5.3f;
@@ -38,24 +40,32 @@ namespace Yarn.Unity.Example {
 
         public float interactionRadius = 2.0f;
 
-        public float movementFromButtons {get;set;}
+        public float movementFromButtons { get; set; }
 
         /// Draw the range at which we'll start talking to people.
-        void OnDrawGizmosSelected() {
+        void OnDrawGizmosSelected()
+        {
             Gizmos.color = Color.blue;
 
             // Flatten the sphere into a disk, which looks nicer in 2D games
-            Gizmos.matrix = Matrix4x4.TRS(transform.position, Quaternion.identity, new Vector3(1,1,0));
+            Gizmos.matrix = Matrix4x4.TRS(transform.position, Quaternion.identity, new Vector3(1, 1, 0));
 
             // Need to draw at position zero because we set position in the line above
             Gizmos.DrawWireSphere(Vector3.zero, interactionRadius);
         }
 
+        private void Awake()
+        {
+            FlagManager.FlagManagerInstance.SetPlayer(gameObject);
+        }
+
         /// Update is called once per frame
-        void Update () {
+        void Update()
+        {
 
             // Remove all player control when we're in dialogue
-            if (FindObjectOfType<DialogueRunner>().IsDialogueRunning == true) {
+            if (FindObjectOfType<DialogueRunner>().IsDialogueRunning == true)
+            {
                 return;
             }
 
@@ -72,8 +82,9 @@ namespace Yarn.Unity.Example {
             transform.position = newPosition;
 
             // Detect if we want to start a conversation
-            if (Input.GetKeyDown(KeyCode.Space)) {
-                CheckForNearbyNPC ();
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                CheckForNearbyNPC();
             }
         }
 
@@ -81,17 +92,19 @@ namespace Yarn.Unity.Example {
         /** Filter them to those that have a Yarn start node and are in range; 
          * then start a conversation with the first one
          */
-        public void CheckForNearbyNPC ()
+        public void CheckForNearbyNPC()
         {
-            var allParticipants = new List<NPC> (FindObjectsOfType<NPC> ());
-            var target = allParticipants.Find (delegate (NPC p) {
-                return string.IsNullOrEmpty (p.talkToNode) == false && // has a conversation node?
+            var allParticipants = new List<NPC>(FindObjectsOfType<NPC>());
+            var target = allParticipants.Find(delegate (NPC p)
+            {
+                return string.IsNullOrEmpty(p.talkToNode) == false && // has a conversation node?
                 (p.transform.position - this.transform.position)// is in range?
                 .magnitude <= interactionRadius;
             });
-            if (target != null) {
+            if (target != null)
+            {
                 // Kick off the dialogue at this node.
-                FindObjectOfType<DialogueRunner> ().StartDialogue (target.talkToNode);
+                FindObjectOfType<DialogueRunner>().StartDialogue(target.talkToNode);
             }
         }
     }
--- TimeTravelGame/Assets/Scripts/FlagManager.cs
+++ TimeTravelGame/Assets/Scripts/FlagManager.cs
@@ -32,7 +32,35 @@ public class FlagManager : MonoBehaviour
     //InTime Variable Information
     public Dictionary<string, bool> InTimeVariables;
 
+    //Save Information
+    public class SaveState
+    {
+        public string saveName;
+        public int activeSceneIndex;
+        public Vector2 position;
+        public Dictionary<string, bool> inTimeFlags;
+        public SaveState(string name, int sceneIndex, Vector2 pos, Dictionary<string, bool> flags)
+        {
+            saveName = name;
+            activeSceneIndex = sceneIndex;
+            position = pos;
+            inTimeFlags = flags;
+        }
+        public override string ToString()
+        {
+            string toReturn = "Save Name: " + saveName + "\nSaved in Scene " + activeSceneIndex + " with player at position " + position;
+            return toReturn;
+        }
+    }
+    public List<SaveState> saves;
+
     public int startSceneNumber = 1;
+
+    private GameObject playerReference;
+    public void SetPlayer(GameObject player)
+    {
+        playerReference = player;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -47,9 +75,36 @@ public class FlagManager : MonoBehaviour
         }
 
         InTimeVariables = new Dictionary<string, bool>();
-
         Debug.Log("Global Variables Length = " + GlobalVariables.Count);
+
+        saves = new List<SaveState>();
         loadIntoScene(startSceneNumber);
+    }
+
+    public bool CreateSaveState()
+    {
+        //SWITCH LOCATION FOR ACTUAL PLAYER LOCATION WHEN AVAILABLE
+        SaveState toAdd = new SaveState("Save " + saves.Count, SceneManager.GetActiveScene().buildIndex, playerReference.transform.position, InTimeVariables);
+        saves.Add(toAdd);
+        Debug.Log("Saved " + toAdd.ToString());
+        return true;
+    }
+    public bool CreateSaveState(string saveName)
+    {
+        bool match = false;
+        foreach (var state in saves)
+        {
+            if (state.saveName == saveName) { match = true; }
+        }
+        if (!match)
+        {
+            //SWITCH LOCATION FOR ACTUAL PLAYER LOCATION WHEN AVAILABLE
+            SaveState toAdd = new SaveState(saveName, SceneManager.GetActiveScene().buildIndex, playerReference.transform.position, InTimeVariables);
+            saves.Add(toAdd);
+            Debug.Log("Saved " + toAdd.ToString());
+            return true;
+        }
+        else { return false; }
     }
 
     private void loadIntoScene(int sceneNumber)
@@ -57,11 +112,16 @@ public class FlagManager : MonoBehaviour
         SceneManager.LoadScene(sceneNumber);
     }
 
-    private void Update() {
+    private void Update()
+    {
         //Testing Stuff
-        if(Input.GetKeyDown(KeyCode.K))
+        if (Input.GetKeyDown(KeyCode.K))
         {
             loadIntoScene(startSceneNumber);
+        }
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            CreateSaveState();
         }
     }
 }
